e8ebc7c01565923089c596cc2fc4b994e8bbcf39
Tito Ebeniro <ebenirot@gmail.com>
2022-03-10 23:50:23 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print an output using puts
+ * Return: 0 if code runs properly
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
