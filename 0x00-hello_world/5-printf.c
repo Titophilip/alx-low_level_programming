2ad6317e2e2b01fa63ba0b92cb950ecd57159f10
Tito Ebeniro <ebenirot@gmail.com>
2022-03-10 23:48:33 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*  main - Print an output using printf */
+/**
+ * main - Print an output using printf
+ * Return: 0 if code runs properly
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
