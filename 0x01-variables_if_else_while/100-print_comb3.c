f47016942876607373af85e01bbb867445113902
Titophilip <ebenirot@gmail.com>
2022-03-12 23:55:05 +0100

    Unique number combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-/* *
-* main - Get unique combinations of numbers between 0-9
-* Return: 0 if successful
-*/
-int main()
+/**
+ * main - Get unique combinations of numbers between 0-9
+ * Return: 0 if successful
+ */
+int main(void)
 {
     int i;
     int j;
-	for (i = 0; i <= 9; i++)
-	{
-	    for (j = i + 1; j <= 9; j++)
-	    {
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
             putchar(i + '0');
             putchar(j + '0');
             if ((i * 10) + j < 89)
             {
                 putchar(',');
                 putchar(' ');
-	        }
-	    }
-	}
-	putchar('\n');
+            }
+        }
+    }
+    putchar('\n');
     return 0;
 }
