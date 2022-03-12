9fc300e055089862779252dff607aa9d84674003
Titophilip <ebenirot@gmail.com>
2022-03-12 23:49:03 +0100

    Unique number combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,25 +3,21 @@
 * main - Get unique combinations of numbers between 0-9
 * Return: 0 if successful
 */
-int main(void)
+int main()
 {
-	/* your code goes there */
-	int i;
-	int j;
-	for (i = 0; i <= 9; i++)
+	for (int i = 0; i <= 9; i++)
 	{
-	    for (j = i + 1; j <= 9; j++)
+	    for (int j = i + 1; j <= 9; j++)
 	    {
-	        putchar(i + '0');
-        	putchar(j + '0');
-        	if ((i * 10) + j < 89)
-	       {
-	           putchar(',');
-	           putchar(' ');
-
-	       }
-	   }
+            putchar(i + '0');
+            putchar(j + '0');
+            if ((i * 10) + j < 89)
+            {
+                putchar(',');
+                putchar(' ');
+	        }
+	    }
 	}
 	putchar('\n');
-	return (0);
+    return 0;
 }
