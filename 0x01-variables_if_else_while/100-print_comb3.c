b6a5b9594154e730e9d7fd815ac65ea690c7a835
Titophilip <ebenirot@gmail.com>
2022-03-13 23:03:57 +0100

    Number combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
         {
             putchar(i + '0');
             putchar(j + '0');
-            if ((i * 10) + j < 89)
+            if ((10 * i) + j < 89)
             {
                 putchar(',');
                 putchar(' ');
