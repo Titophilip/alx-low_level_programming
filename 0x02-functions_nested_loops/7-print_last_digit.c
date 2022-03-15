ae5a58c48271028e18ac60d3415cf69e1086c4db
Titophilip <ebenirot@gmail.com>
2022-03-15 23:42:53 +0100

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,11 @@ int print_last_digit(int n)
 {
 	int c;
 	
-	if (n < 0)
+	c = n % 10;
+	if (c < 0)
 	{
-		n = -n;
-		c = n % 10;
-		if (c < 0)
-		{
-			c = -c;
-		}
-		_putchar(c + '0');
+		c = -c;
 	}
+	_putchar(c + '0');
 	return (c);
 }
