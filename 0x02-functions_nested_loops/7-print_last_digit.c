1a28b5c6007532f0e505adf9c1825115089152f7
Titophilip <ebenirot@gmail.com>
2022-03-15 23:44:46 +0100

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,14 @@
  * print_last_digit - print last
  * digits of a given number n
  *
+ * @n - parameter to be tested
+ *
  * Return: 0
  */
 int print_last_digit(int n)
 {
 	int c;
-	
+
 	c = n % 10;
 	if (c < 0)
 	{
