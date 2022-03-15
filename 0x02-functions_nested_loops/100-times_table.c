f182af85d5d4456c853d8ee0d13497653cb6089a
Titophilip <ebenirot@gmail.com>
2022-03-16 00:36:13 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ *times_table - 9 times table
+ *
+ * @n: Number to be multiplied up to.
+ *
+ *Return: no return value
+ *
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return (0);
+	}
+	int i, j, product;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			product = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
