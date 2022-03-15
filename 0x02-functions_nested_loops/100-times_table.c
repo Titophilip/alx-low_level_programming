77d7a6b6a29a153f98c1da92b6ca430772415145
Titophilip <ebenirot@gmail.com>
2022-03-16 00:42:45 +0100

    Nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,38 +9,38 @@
  */
 void print_times_table(int n)
 {
+	int i, j, product;
 	if (n < 0 || n > 15)
 	{
 		return (0);
-	}
-	int i, j, product;
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
+	} else {
+		for (i = 0; i < n; i++)
 		{
-			product = i * j;
-			if (j == 0)
+			for (j = 0; j < n; j++)
 			{
-				_putchar('0' + product);
+				product = i * j;
+				if (j == 0)
+				{
+					_putchar('0' + product);
+				}
+				else if (product >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				if (j != 9)
+				{
+					_putchar(',');
+				}
 			}
-			else if (product >= 10)
-			{
-				_putchar(' ');
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			if (j != 9)
-			{
-				_putchar(',');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
