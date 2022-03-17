b72acfea8309a8da6b5a5a5d8e8cba4b52287702
Titophilip <ebenirot@gmail.com>
2022-03-17 23:05:36 +0100

    Nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper: checks if the character is 
+ * in upper case
+ * @c: this is the character to be checked.
+ *
+ * Retutn: 1 if c is uppercase
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+/**
+ * main - prints the fizzbuzz classification
+ *
+ * return - always zero (0)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz");
+			if (i == 100)
+				printf("\n");
+			else
+				printf(" ");
+		}
+		else {
+			printf("%d ", i);;
+		}
+	}
+	return (0);
+}
